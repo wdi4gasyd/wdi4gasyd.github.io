da75f42af879930e5bf67feaa4c33cc66862dd15
thomasdane <tjdane@gmail.com>
2014-05-13 00:26:58 +1000

    enable sprites to walk left and right depending on location

--- js/views/studentListView.js
+++ js/views/studentListView.js
@@ -20,16 +20,14 @@ yearBook.StudentListView = Backbone.View.extend({
     this.$el.addClass(this.model.get('slug'));
     // give the element a random starting position on the stage
     this.leftPos = ( Math.floor(Math.random() * ( this.windowWidth - 32) ) )
-    this.$el.css({'left': this.leftPos + 'px', 'border': '1px solid #FFF'});
+    this.$el.css({'left': this.leftPos + 'px'});
     thisName = this.model.get('slug');
     // NEXT - add a function for walk left, walk right, and some way of selecting the correct function
     // randomly apply walkLeft or walkRight to start them off
     if ((parseInt(this.$el.css('left')) % 2) == 0 ) {
-      this.walkTime('left', thisName);
+      window.setInterval(_.bind(this.walkLeft, this), 50);
     } else {
-      console.log(this.$el.css('left'))
-      console.log('ODD');
-      this.walkTime('right', thisName);
+      window.setInterval(_.bind(this.walkRight, this), 50);
     }
   },
 
@@ -40,35 +38,22 @@ yearBook.StudentListView = Backbone.View.extend({
     yearBook.router.navigate(this.model.get('slug'), true);
     console.log('you clicked a student');
   },
-  //tells the sprite to do the walk function at a certain speed
-  walkTime: function(direction){
-    var thisPos = parseInt($(this.el).css('left'));
-    if (direction == 'left'){
-      window.setInterval(_.bind(this.walkLeft, this), 10);
-
-      //this.timer = setInterval(this.walkLeft(thisName), 500);
-    } else {
-      window.setInterval(_.bind(this.walkRight, this), 10);
-    }
-  },
-
+  
   walkLeft: function(){
-    var thisPos = parseInt($(this.el).css('left'));
-    // if (thisPos > 100){    
-       (this.$el).css({'left': (thisPos - 1) + 'px', 'border': 'solid 3px #F40'}) 
-    //    console.log(thisPos)     
-    // } else {
-    // this.walkRight()
-    // }
-    
+    var thisPos = parseInt($(this.el).css('left'));    
+    (this.$el).css({'left': (thisPos - 1) + 'px'})
+    if (thisPos < 100) {
+      window.setInterval(_.bind(this.walkRight, this), 1000);           
+    } 
+
   },
 
   walkRight: function(){
-    var thisPos = parseInt($(this.el).css('left'));
-    // if (thisPos < 900){    
-       (this.$el).css({'left': (thisPos + 1) + 'px', 'border': 'solid 3px #F40'})
-    //    console.log(thisPos)       
-    // } 
-         
+    var thisPos = parseInt($(this.el).css('left'));     
+    (this.$el).css({'left': (thisPos + 1) + 'px'})
+    if (thisPos > 850) {
+      window.setInterval(_.bind(this.walkLeft, this), 1000);           
+    }      
+ 
   }
 });
