d20585fb9e69947a85bceddfeec35d18d7a5184b
=-kriss-= <kriss@modelcitizen.com.au>
2014-05-28 14:45:22 +1000

    walk direction sorted - next up is change sprite direction

--- js/views/studentListView.js
+++ js/views/studentListView.js
@@ -19,18 +19,15 @@ yearBook.StudentListView = Backbone.View.extend({
     // add the student name to the containing div (this.$el)
     this.$el.addClass(this.model.get('slug'));
     // give the element a random starting position on the stage
-    this.leftPos = ( Math.floor(Math.random() * ( this.windowWidth - 32) ) )
+    this.leftPos = ( Math.floor(Math.random() * ( this.windowWidth - 32) ) );
     this.$el.css({'left': this.leftPos + 'px', 'border': '1px solid #FFF'});
     thisName = this.model.get('slug');
     // NEXT - add a function for walk left, walk right, and some way of selecting the correct function
     // randomly apply walkLeft or walkRight to start them off
-    if ((parseInt(this.$el.css('left')) % 2) == 0 ) {
-      console.log();
-      this.walkTime('left', thisName);
+    if ((parseInt(this.$el.css('left')) % 2) === 0 ) {
+      this.walkTime('left', this.$el);
     } else {
-      console.log(this.$el.css('left'))
-      console.log('ODD');
-      this.walkTime('right', thisName);
+      this.walkTime('right', this.$el);
     }
   },
 
@@ -42,40 +39,68 @@ yearBook.StudentListView = Backbone.View.extend({
     console.log('you clicked a student');
   },
 
-  walkTime: function(direction){
-    if (direction == 'left'){
-      window.setInterval(_.bind(this.walkLeft, this), 100);
-      //this.timer = setInterval(this.walkLeft(thisName), 500);
-    } else {
-      window.setInterval(_.bind(this.walkRight, this), 100);
-    }
-  },
+  walkTime: function(direction, student) {
+    student.direction = direction
 
-  walkLeft: function(){
-    var thisPos = parseInt($(this.el).css('left'));
-    var walkSwitch = ''
-    if (thisPos <= 0 ){
-      this.walkSwitch = 'right'
-      // this.unbind(this.walkLeft, this)
-      // this.walkRight()
-    }
-    if (thisPos > (960 - 32) ){
-      this.walkSwitch = 'left'
-    }
-    if (this.walkSwitch == 'left'){
-      (this.$el).css({'left': (thisPos + 1) + 'px', 'border': 'solid 3px #F40'})
-    } else {
-      (this.$el).css({'left': (thisPos - 1) + 'px', 'border': 'solid 3px #F40'})
-    }
-  },
 
-  walkRight: function(){
-    var thisPos = parseInt($(this.el).css('left'));
-    if (thisPos > (960 - 32) ){
-      //this.unbind(this.walkRight, this)
-      this.walkLeft()
-    } else {
-      $(this.el).css({'left': (thisPos + 1) + 'px', 'border': 'solid 3px #1F0'})
-    }
+
+
+    setInterval(function () {
+      student.thisPos = parseInt(student.css('left') );
+      if (student.thisPos <= 0 ) {
+        student.direction = 'right'
+        console.log('limit - now go ' + student.direction)
+      }
+      if (student.thisPos >= 928 ) {
+        student.direction = 'left'
+        console.log('limit - now go ' + student.direction)
+      }
+
+      if (student.direction === 'left') {
+        student.css({
+          'left': (student.thisPos - 1) + 'px',
+          'border' : 'solid 3px #F40'
+        });
+      } else {     
+        student.css({
+          'left': (student.thisPos + 1) + 'px',
+          'border' : 'solid 3px #F09'
+        });
+      }
+
+      
+
+      
+
+
+
+
+    }, 100)
   }
+  // walkLeft: function(){
+  //   var thisPos = parseInt($(this.el).css('left'));
+  //   var walkSwitch = ''
+  //   if (thisPos <= 0 ){
+  //     this.walkSwitch = 'right'
+  //     // this.unbind(this.walkLeft, this)
+  //     // this.walkRight()
+  //   }
+  //   if (thisPos > (960 - 32) ){
+  //     this.walkSwitch = 'left'
+  //   }
+  //   if (this.walkSwitch == 'left'){
+  //     (this.$el).css({'left': (thisPos + 1) + 'px', 'border': 'solid 3px #F40'})
+  //   } else {
+  //     (this.$el).css({'left': (thisPos - 1) + 'px', 'border': 'solid 3px #F40'})
+  //   }
+  // },
+
+  // walkRight: function(){
+  //   var thisPos = parseInt($(this.el).css('left'));
+  //   if (thisPos > (960 - 32) ){
+  //     //this.unbind(this.walkRight, this)
+  //     this.walkLeft()
+  //   } else {
+  //     $(this.el).css({'left': (thisPos + 1) + 'px', 'border': 'solid 3px #1F0'})
+  //   }
 });
