858e28f34840998592c60514985dc86f1ec2b3ef
markpereira <mark.pereira@xtra.co.nz>
2014-06-05 13:10:17 +1200

    update markSpeak function

--- js/game.js
+++ js/game.js
@@ -588,7 +588,7 @@ var runGame = function (){
     
         // Samples an array of Mark's best comments
         markStyle = { font: "20px Arial", fill: "#fff", align: "center" };
-        var markisms = ["\"This is Mark.\"", "\"I did the WDI course.\"", "\"Marks words of wisdom\""]
+        var markisms = ["\"Hi! I'm Mark.\"", "\"I like to talk about how you are feeling and stuff.\"", "\"ooh..I have white hair in my beard. This means one of two things.. either I'm getting old... OR...  I'm becoming a Wizard!\""]
         var markText = game.add.text(1720, 1300, _.sample(markisms), markStyle);  
         Mark.animations.play('talk');
         winChecker(Mark.key);
