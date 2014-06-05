d77886196d023ff8ac525b570af019f30575c23b
markpereira <mark.pereira@xtra.co.nz>
2014-06-05 13:03:19 +1200

    modify simonwording placement take 5

--- js/game.js
+++ js/game.js
@@ -612,7 +612,7 @@ var runGame = function (){
         Simon.animations.play('talk');
         winChecker(Simon.key);
         // Call removeText function after a couple seconds
-        setTimeout(removeText, 2000);
+        setTimeout(removeText, 1500);
 
         function removeText () {
             game.world.remove(simonText);
